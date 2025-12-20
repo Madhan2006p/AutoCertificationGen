ba72ce25513b8b3ba6120e2a0b943d51a45f322e
Madhan2006p <madhanp28072006@gmail.com>
2025-12-20 12:47:02 +0530

    Bug Fixed from certificate

--- backend/genCertificate.py
+++ backend/genCertificate.py
@@ -1,13 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont
+import os
+BASE_DIR = os.path.dirname(__file__)
+FONT_DIR = os.path.join(BASE_DIR, "fonts")
 
 def generate_certificate(name, roll_no, event):
     img = Image.open("backend/certificate_template.png")
     draw = ImageDraw.Draw(img)
 
     # Fonts
-    font_name = ImageFont.truetype("C:/Windows/Fonts/arialbd.ttf", 70)
-    font_event = ImageFont.truetype("C:/Windows/Fonts/arialbd.ttf", 85)
-    font_small = ImageFont.truetype("C:/Windows/Fonts/arial.ttf", 45)
+    font_name = ImageFont.truetype(
+        os.path.join(FONT_DIR, "DejaVuSans-Bold.ttf"), 70
+    )
+    font_event = ImageFont.truetype(
+        os.path.join(FONT_DIR, "DejaVuSans-Bold.ttf"), 85
+    )
+    font_small = ImageFont.truetype(
+        os.path.join(FONT_DIR, "DejaVuSans.ttf"), 45
+    )
+
 
     # Text values
     name_reg = f"{name.upper()} ({roll_no})"
